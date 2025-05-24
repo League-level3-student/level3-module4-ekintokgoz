6f55999a2f341d8cf2da553a18d3ec91465b5398
league <league@imac-24.attlocal.net>
2025-05-24 12:58:11 -0700

    continue textundoredo

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -9,14 +9,33 @@ public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
-
+    	
+    		Stack<Double> numbers = new Stack<Double>();
+    	
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-
+    		Random ran = new Random(100);
+    		for(int i = 0; i < 100; i++) {
+    			numbers.push(ran.nextDouble() * 100);
+    		}
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-
+    		String num1 = JOptionPane.showInputDialog("Enter a number between 0 and 100");
+    		String num2 = JOptionPane.showInputDialog("Enter another number between 0 and 100");
+    		double n1 = Double.parseDouble(num1);
+    		double n2 = Double.parseDouble(num2);
+    		if(n2 < n1) {
+    			double n3 = n1;
+    			n1 = n2;
+    			n2 = n3;
+    		}
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
-        //    between the two numbers entered by the user, print it to the screen.
-
+        //    between the two numbers entered by the user, print it to the screen.	
+    		
+    		while(! numbers.isEmpty()) {
+    			Double topOfStack = numbers.pop();
+    			if(topOfStack >= n1 && topOfStack <= n2) {
+    				System.out.println(topOfStack);
+    			}
+    		}
 
         // EXAMPLE:
         // NUM 1: 65
--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,9 +1,13 @@
 package _00_IntroToStacks;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class _02_TextUndoRedo {
     /* 
      * Create a JFrame with a JPanel and a JLabel.
-     * 
+     *
      * Every time a key is pressed, add that character to the JLabel. It should
      * look like a basic text editor.
      * 
@@ -16,6 +20,22 @@ public class _02_TextUndoRedo {
      * pressed, the top Character is popped  off the Stack and added back to
      * the JLabel.
      */
-
+		
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JLabel label = new JLabel();
+		
+		public static void main(String[] args) {
+			 _02_TextUndoRedo x = new _02_TextUndoRedo();
+			 x.run();
+			 
+		}
+		
+		public void run() {
+			frame.add(panel);
+			panel.add(label);
+			
+			
+		}
 
 }
