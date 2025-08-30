64230dd23a4f5dd1115ec38a4e4a056ed7ba36bb
league <bobby1545>
2025-08-30 12:56:41 -0700

    continue hangman

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -1,5 +1,53 @@
 package _03_Hangman;
 
-public class Hangman {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class Hangman implements KeyListener {
+	
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel("Lives: ");
+	
+	public static void main(String[] args) {		
+		Hangman hangman = new Hangman();
+		hangman.run();	
+	}
+	
+	public void run() {
+		frame.add(panel);
+		panel.add(label);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(this);
+	
+		
+		String numOfWords = JOptionPane.showInputDialog("Enter the number of words to guess (1-100): ");
+		Stack<String> hangman = new Stack<String>();
+		hangman.
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
 }
