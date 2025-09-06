cef7a27fe81056bee997daae87270ad2b0bbdc64
league <bbb>
2025-09-06 12:56:59 -0700

    hangman - loop through number of words

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -13,7 +13,11 @@ public class Hangman implements KeyListener {
 	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
-	JLabel label = new JLabel("Lives: ");
+	int lives = 10;
+	JLabel label = new JLabel("Lives: " + lives);
+	JLabel wordLabel = new JLabel();
+	String word = "";
+	String hiddenWord = "";
 	
 	public static void main(String[] args) {		
 		Hangman hangman = new Hangman();
@@ -23,20 +27,44 @@ public class Hangman implements KeyListener {
 	public void run() {
 		frame.add(panel);
 		panel.add(label);
-		frame.pack();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.addKeyListener(this);
-	
+			
 		
 		String numOfWords = JOptionPane.showInputDialog("Enter the number of words to guess (1-100): ");
 		Stack<String> hangman = new Stack<String>();
-		hangman.
+		word = Utilities.readRandomLineFromFile("dictionary.txt");
+		for(int i = 0; i < word.length(); i++) {
+			hiddenWord += "_";
+		}
+		wordLabel.setText(hiddenWord);
+		panel.add(wordLabel);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
+		if(word.contains(e.getKeyChar()+"")) {
+			StringBuilder sb = new StringBuilder(hiddenWord);
+			for(int i = 0; i < word.length(); i++) {
+				if(word.charAt(i) == e.getKeyChar()) {
+					sb.setCharAt(i, e.getKeyChar());
+				}		
+			}
+			hiddenWord = sb.toString();
+			wordLabel.setText(hiddenWord);
+		}else {
+			lives -= 1;
+			label.setText("Lives: " + lives);
+		}
+		if(lives == 0) {
+			JOptionPane.showMessageDialog(null, "Ran out of lives. The hidden word was: " + word);
+		}
+		if(!hiddenWord.contains("_")) {
+			JOptionPane.showMessageDialog(null, "You found the hidden word!");
+			
+		}
 	}
 
 	@Override
